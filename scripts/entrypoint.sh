122ebffbb894f37cc732c4588c294213b7e31357
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-08-21 10:46:24 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-cd playbooks/vars
+cd playbooks
+ansible-playbook playbook.yml
 
-for dir in */; do
-   cd "$dir"
-   path=$(pwd)
-   mkdir deploy
-   cp -r $path/addresses.yaml deploy/addresses.yaml
-   cp -r $path/devices.yaml deploy/devices.yaml
-   ls deploy
-   cp ../../deploy-branch-readyaml.yaml deploy/deploy-branch-readyaml.yaml
-   ansible-playbook ./deploy/deploy-branch-readyaml.yaml --extra-vars "auth=$auth org=Demo"
-   cd ..
-done
- 
