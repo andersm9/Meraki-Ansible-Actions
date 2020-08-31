cbcd17feba16ebe2da63bb1d4d8450b6e493d1c1
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-08-31 13:30:31 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@ pip install boto3
 mkdir testdir
 ls
 cd playbooks
-ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID"
+ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID AWS_DEFAULT_REGION=eu-west-1"
