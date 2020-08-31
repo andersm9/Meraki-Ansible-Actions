db6e31024725ff55aa72699718dca96cfe702f1c
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-08-31 12:06:35 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,4 +3,4 @@
 mkdir testdir
 ls
 cd playbooks
-ansible-playbook playbook.yml --extra-vars "auth=$uth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID"
+ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID"
