7b8edd4b9034b6c6c6f6eba566df30fe07462543
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-09-04 11:25:28 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,13 @@ pip install botocore
 mkdir testdir
 ls
 cd playbooks
+echo "-----------------------------------------------"
+echo "TESTSECRET"
 echo $TESTSECRET
+echo "AWS_ACCESS_KEY_ID"
+echo $AWS_ACCESS_KEY_ID
+echo "AWS_SECRET_ACCESS_KEY"
+echo $AWS_SECRET_ACCESS_KEY
+echo "region"
+echo $REGION
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY region=$REGION TESTSECRET=$TESTSECRET"
