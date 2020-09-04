2b92ffabf046fba26f5429f9532806b13c9737aa
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-09-04 16:58:22 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 pip install boto3
 pip install botocore
+cd playbooks
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY"
