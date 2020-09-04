c074f840a54a58b271a5f2102401503a12695fce
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-09-04 12:59:12 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -13,4 +13,6 @@ echo "AWS_SECRET_ACCESS_KEY"
 echo $AWS_SECRET_ACCESS_KEY
 echo "region"
 echo $REGION
+echo "nonsense"
+echo $nonsense
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID =$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY region=$REGION TESTSECRET=$TESTSECRET"
