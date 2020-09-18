6c930ed796ec1fef968fabf4b9234676ba5df2a7
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-09-18 16:06:39 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,6 +2,6 @@
 pip install boto3
 pip install botocore
 cd playbooks
-ssh -i $SSH_KEY ubuntu@ec2-54-171-108-150.eu-west-1.compute.amazonaws.com
+ssh -i $SSH_KEY -J ubuntu@ec2-54-171-108-150.eu-west-1.compute.amazonaws.com
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY PSK=$PSK RTID=$RTID SSH_KEY=$SSH_KEY"
 echo ping 172.31.26.184
