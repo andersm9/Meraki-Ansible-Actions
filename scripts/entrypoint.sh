86c6d762823a78aaa0ee83a76e198cfe48c7b704
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-10-02 13:01:09 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,3 +3,4 @@ pip install boto3
 pip install botocore
 cd playbooks
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY PSK=$PSK RTID=$RTID"
+aws ssm send-command --document-name "AWS-RunPowerShellScript" --instance-ids "i-08793cba603e6ef4f" --region eu-west-1 --parameters commands='ping -c 2 bbc.co.uk'
