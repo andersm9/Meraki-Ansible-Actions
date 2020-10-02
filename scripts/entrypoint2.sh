68d954c08916e4545a252a346f031310f1ffbf4a
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-10-02 11:08:15 +0100

    Update entrypoint2.sh

--- scripts/entrypoint2.sh
+++ scripts/entrypoint2.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-pip install boto3
-pip install botocore
-cd playbooks
-ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY PSK=$PSK RTID=$RTID"
-ping -c 1 google.com
 aws ssm get-command-invocation --command-id cbc8fe18-b75e-4b1b-b2bc-211601a2229b --region eu-west-1 --instance-id i-08793cba603e6ef4f
