fe266cd3ea9d9a80ecbd1d7357b079d36adb90d6
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-10-02 15:07:16 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,5 +4,6 @@ pip install botocore
 cd playbooks
 ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY PSK=$PSK RTID=$RTID"
 cmdid=$(aws ssm send-command --document-name "AWS-RunPowerShellScript" --instance-ids "i-08793cba603e6ef4f" --region eu-west-1 --parameters commands='ping -c 2 bbc.co.uk' --query "Command.CommandId" --output text)
-aws ssm list-command-invocations --command-id "$cmdid" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text --region eu-west-1 --instance-id i-08793cba603e6ef4f
+output=$(aws ssm list-command-invocations --command-id "$cmdid" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text --region eu-west-1 --instance-id i-08793cba603e6ef4f)
+echo $output
 echo "end of entrypoint.sh"
