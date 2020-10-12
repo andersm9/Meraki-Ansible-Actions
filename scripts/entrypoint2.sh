2885dea7a93b76eb0f3ca4c966f773b602e8901f
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-10-12 20:57:54 +0100

    Update entrypoint2.sh

--- scripts/entrypoint2.sh
+++ scripts/entrypoint2.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ssm get-command-invocation --command-id cbc8fe18-b75e-4b1b-b2bc-211601a2229b --region eu-west-1 --instance-id i-08793cba603e6ef4f
+aws ssm get-command-invocation --command-id cbc8fe18-b75e-4b1b-b2bc-211601a2229b --region eu-west-1 --instance-id i-0b8d9c6ca369c6e34
