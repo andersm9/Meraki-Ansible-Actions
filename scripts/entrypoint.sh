54c205cf72e167b2009ba87f3de7cb97d8264216
andersm9 <m_anderson9@YAHOO.CO.UK>
2020-10-14 16:11:56 +0100

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,5 @@ ansible-playbook playbook.yml --extra-vars "auth=$auth AWS_ACCESS_KEY_ID=$AWS_AC
 cmdid=$(aws ssm send-command --document-name "AWS-RunPowerShellScript" --instance-ids "i-0b8d9c6ca369c6e34" --region eu-west-1 --parameters commands='ping -c 2 bbc.co.uk' --query "Command.CommandId" --output text)
 output=$(aws ssm list-command-invocations --command-id "$cmdid" --details --query "CommandInvocations[*].CommandPlugins[*].Output[]" --output text --region eu-west-1 --instance-id i-0b8d9c6ca369c6e34)
 echo $output
+echo $RTID
 echo "end of entrypoint.sh"
